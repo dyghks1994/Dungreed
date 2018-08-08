307af12a585c1aeebed14a3bd65275c59a08a249
YoHwanChoi <dyghks1994@gmail.com>
2018-08-08 21:51:50 +0900

    캐릭터 클래스 수정
    _hp -> _maxHp, currentHp 로 세분화 및 겟터세터 수정

--- Project/Dungreed/Dungreed/character.h
+++ Project/Dungreed/Dungreed/character.h
@@ -3,23 +3,27 @@ class character
 {
 private:
 	image*	_image;		  // 캐릭터 이미지
-	int		_hp;		  // 생명력
-	int		_attack;	  // 공격력
+
+	int		_maxHp;		  // 최대 생명력
+	int		_currentHp;	  // 현재 생명력
+	int		_atk;		  // 공격력
 	int		_def;		  // 방어력
 	float	_moveSpeed;	  // 이동속도
 	float	_jumpPower;	  // 점프력
-	float	_attackSpeed; // 공격속도
+	float	_atkSpeed;	  // 공격속도
 
 public:
 	character();
 	~character();
 
 	// getter setter
-	int getHp() { return _hp; }
-	void setHp(int hp) { _hp = hp; }
+	int getMaxHp() { return _maxHp; }
+	void setMaxHp(int maxHp) { _maxHp = maxHp; }
+	int getCurrentHp() { return _currentHp; }
+	void setCurrnetHp(int currentHp) { _maxHp = currentHp; }
 
-	int getAttack() { return _attack; }
-	void setAttack(int attack) { _attack = attack; }
+	int getAtk() { return _atk; }
+	void setAtk(int atk) { _atk = atk; }
 
 	int getDef() { return _def; }
 	void setDef(int def) { _def = def; }
@@ -30,8 +34,8 @@ public:
 	float getJumpPower() { return _jumpPower; }
 	void setJumpPower(float jumpPower) { _jumpPower = jumpPower; }
 
-	float getAttackSpeed() { return _attackSpeed; }
-	void setAttackSpeed(float attackSpeed) { _attackSpeed = attackSpeed; }
+	float getAtkSpeed() { return _atkSpeed; }
+	void setAtkSpeed(float atkSpeed) { _atkSpeed = atkSpeed; }
 
 };
 
