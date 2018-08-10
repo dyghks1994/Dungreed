e50f56d34098433f71fe7cba5cf64ebdeea5fd50
YoHwanChoi <dyghks1994@gmail.com>
2018-08-10 16:35:48 +0900

    카메라 기능 구현 완료
    카메라 기능의 핵심 부분을 구현 완료
    (이제 배경과 캐릭터 이미지 입히고 그에 따른 수정만 조금 해주면 됨)

--- Project/Dungreed/Dungreed/gameScene.cpp
+++ Project/Dungreed/Dungreed/gameScene.cpp
@@ -25,6 +25,8 @@ HRESULT gameScene::init()
 
 	_cameraX = _x - WINSIZEX / 2;
 	_cameraY = _y - WINSIZEY / 2;
+	_cameraHorizontalMoveOk = false;
+	_cameraVerticalMoveOk = false;
 
 	return S_OK;
 }
@@ -35,36 +37,7 @@ void gameScene::release()
 
 void gameScene::update()
 {
-	if (KEYMANAGER->isStayKeyDown(VK_LEFT))
-	{
-		_x -= 3;
-		_cameraX -= 3;
-	}
-	if (KEYMANAGER->isStayKeyDown(VK_RIGHT))
-	{
-		_x += 3;
-		_cameraX += 3;
-	}
-	if (KEYMANAGER->isStayKeyDown(VK_UP))
-	{
-		_y -= 3;
-		_cameraY -= 3;
-	}
-	if (KEYMANAGER->isStayKeyDown(VK_DOWN))
-	{
-		_y += 3;
-		_cameraY += 3;
-	}
-
-	if (_x - 25 <= 0) _x = 25;
-	if (_x + 25 >= BACKDCSIZEX) _cameraX = _x = BACKDCSIZEX - 25;
-	if (_y - 25 <= 0) _y = 25;
-	if (_y + 25 >= BACKDCSIZEY) _cameraY = _y = BACKDCSIZEY - 25;
-
-	if (_cameraX <= 0) _cameraX = 0;
-	if (_cameraY <= 0) _cameraY = 0;
-
-	_rc = RectMakeCenter(_x, _y, 50, 50);
+	playerMove();
 }
 
 void gameScene::render()
@@ -77,4 +50,73 @@ void gameScene::render()
 	_backBuffer->render(getMemDC(), 0, 0, _cameraX, _cameraY, WINSIZEX, WINSIZEY);
 	
 	
+}
+
+void gameScene::playerMove()
+{
+	// ************************************* 캐릭터 조정 **********************************************
+	
+	// 좌로 이동
+	if (KEYMANAGER->isStayKeyDown(VK_LEFT))
+	{
+		_x -= 3;	// 플레이어 x좌표 감소
+		if (_cameraHorizontalMoveOk) _cameraX -= 3;	// 카메라 수평이동이 가능한 상태라면 카메라 x좌표 감소
+	}
+
+	// 우로 이동
+	if (KEYMANAGER->isStayKeyDown(VK_RIGHT))
+	{
+		_x += 3;	// 플레이어 x좌표 증가
+		if (_cameraHorizontalMoveOk) _cameraX += 3; // 카메라 수평이동이 가능한 상태라면 카메라 x좌표 증가
+	}
+
+	// 위로 이동
+	if (KEYMANAGER->isStayKeyDown(VK_UP))
+	{
+		_y -= 3;	// 플레이어 y좌표 감소
+		if (_cameraVerticalMoveOk) _cameraY -= 3; // 카메라 수직이동이 가능한 상태라면 카메라 y좌표 감소
+	}
+
+	// 아래로 이동
+	if (KEYMANAGER->isStayKeyDown(VK_DOWN))
+	{
+		_y += 3;	// 플레이어 Y좌표 증가
+		if (_cameraVerticalMoveOk) _cameraY += 3; // 카메라 수직이동이 가능한 상태라면 카메라 y좌표 증가
+	}
+
+	if (KEYMANAGER->isOnceKeyDown(VK_SPACE))
+	{
+
+	}
+
+	if (_x - 25 <= 0) _x = 25;
+	if (_x + 25 >= BACKDCSIZEX) _x = BACKDCSIZEX - 25;
+	if (_y - 25 <= 0) _y = 25;
+	if (_y + 25 >= BACKDCSIZEY) _y = BACKDCSIZEY - 25;
+
+	_rc = RectMakeCenter(_x, _y, 50, 50);
+
+	// end of 캐릭터 조정 *********************************************************************************
+
+
+	// 카메라 조정 ****************************************************************************************
+	// 카메라 좌우 이동 가능한지 판별
+	if (_x <= 0 + WINSIZEX / 2 || _x >= BACKDCSIZEX - WINSIZEX / 2) _cameraHorizontalMoveOk = false;
+	else _cameraHorizontalMoveOk = true;
+
+	// 카메라 상하 이동 가능한지 판별
+	if (_y <= 0 + WINSIZEY / 2 || _y >= BACKDCSIZEY - WINSIZEY / 2) _cameraVerticalMoveOk = false;
+	else _cameraVerticalMoveOk = true;
+
+	// 카메라가 맵을 벗어나지 않도록 위치 조정
+	// 카메라가 왼쪽 끝에 있을 때
+	if (_cameraX <= 0) _cameraX = 0;
+	// 카메라가 오른쪽 끝에 있을 때
+	if (_cameraX + WINSIZEX >= BACKDCSIZEX) _cameraX = BACKDCSIZEX - WINSIZEX;
+	// 카메라가 위쪽 끝에 있을 때
+	if (_cameraY <= 0) _cameraY = 0;
+	// 카메라가 아래쪽 끝에 있을 때
+	if (_cameraX + WINSIZEX >= BACKDCSIZEX) _cameraX = BACKDCSIZEX - WINSIZEX;
+
+	// end of 카메라 조정 **********************************************************************************
 }
--- Project/Dungreed/Dungreed/gameScene.h
+++ Project/Dungreed/Dungreed/gameScene.h
@@ -5,13 +5,15 @@
 class gameScene : public gameNode
 {
 private:
-	image* map;
-	player* _player;
+	image* map;						// 배경 이미지
+	player* _player;				// 플레이어 이미지
 	
 	
-	RECT _rc;
-	int _x, _y;
-	int _cameraX, _cameraY;
+	RECT _rc;						// 테스트용 RECT
+	int _x, _y;						// 플레이어 x, y 좌표
+	int _cameraX, _cameraY;			// 카메라 x, y 좌표
+	bool _cameraHorizontalMoveOk;	// 카메라의 좌우이동 가능 여부
+	bool _cameraVerticalMoveOk;		// 카메라의 상하이동 가능 여부
 
 public:
 	gameScene();
@@ -21,5 +23,7 @@ public:
 	void release();
 	void update();
 	void render();
+
+	void playerMove();
 };
 
